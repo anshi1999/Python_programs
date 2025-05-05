b073be3be457feb0cf19b663a699aee511fe71a7
anshi1999 <141669278+anshi1999@users.noreply.github.com>
2025-05-05 11:29:00 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -7,3 +7,19 @@ elif num >=0:
     print("positive")
 else:
     print("negative")
+
+
+#NESTED IF-ELSE
+num = int(input("Num= "))
+if num>=0:
+    if num !=0:
+        print("positive")
+    else:
+        print("Zero")
+else:
+    print("negative")
+
+
+#TERNARY APPROACH
+num = int(input("Num= "))
+print("positive" if num>=0 else "negative")

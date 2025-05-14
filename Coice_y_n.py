74a943d03314c6b7e0b099a82a1fe19672e8b6d3
anshi1999 <141669278+anshi1999@users.noreply.github.com>
2025-05-14 12:50:17 +0530

    Create Coice_y_n.py

---
+++ Coice_y_n.py
@@ -0,0 +1,9 @@
+while True :
+    a = float(input("Num1 = "))
+    b = float(input("Num2 = "))
+    print(a*b)
+    choice = input("Do you want to continue: y/n= ")
+    if choice == "y":
+        continue
+    else:
+        break

0d9d402f293fa56fd500325c07ca9a46c3868f25
anshi1999 <141669278+anshi1999@users.noreply.github.com>
2025-05-14 17:55:18 +0530

    Create Even_odd_1to100.py

---
+++ Even_odd_1to100.py
@@ -0,0 +1,29 @@
+# Even from 1 to 100
+i = 0
+while i<=100:
+    if i%2 ==0:
+        print(i)
+    i+=1
+
+
+#Odd from 1 to 100
+i = 0
+while i<=100:
+    if i%2 !=0:
+        print(i)
+    i+=1
+
+
+#Even from 100 to 1
+i = 100
+while i>=0:
+    if i%2 ==0:
+        print(i)
+    i-=1
+
+#Odd from 100 to 1
+i = 100
+while i>=0:
+    if i%2 !=0:
+        print(i)
+    i-=1

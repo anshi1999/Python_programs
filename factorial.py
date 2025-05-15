294b0d13786b3ccffc45de174ac9f4352906efb1
anshi1999 <141669278+anshi1999@users.noreply.github.com>
2025-05-15 17:54:01 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#Using for
+f=1
+num=int(input('enter num='))
+for i in range(num,0,-1):
+    f=f*i
+print(f)
+
+
+#Using while 
+num = int(input("Num= "))
+
+f = 1
+i = 1
+while i <=num:
+    i+=1
+    f =f*i
+    print(f)
